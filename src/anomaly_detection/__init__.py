a9756ead143c9e1ba68168cccc67ef0c76d7cff4
lemayian23 <lemayianledavit2018@gmail.com>
2025-12-15 06:39:21 +0300

    modified init

--- src/anomaly_detection/__init__.py
+++ src/anomaly_detection/__init__.py
@@ -1,3 +1,6 @@
-"""
-Anomaly detection algorithms for livestock health monitoring
-"""
+# Initialize data validator if enabled
+self.data_validator = None
+validation_config = self.config.get('validation', {})
+if validation_config.get('enabled', False):
+    self.data_validator = get_data_validator(self.config)
+    logger.info("Data validation enabled")
